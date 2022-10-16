21c40e6a1bfd720375c83a8b05c1201c33c77ee6
choiyun9yu <110392046+choiyun9yu@users.noreply.github.com>
2022-10-17 08:42:40 +0900

    JavaScript
    JavaScript

---
+++ 05. jQuery/HorseGame/main.js
@@ -0,0 +1,16 @@
+// 1. left라는 버튼을 눌렀을 때, 말이 왼쪽으로 50px씩 이동
+// 2. right라는 버튼을 눌렀을 때, 말이 오른쪽으로 50px씩 이동
+//  단, 말이 배경사진 밖으로 나가지 않게 할 것!
+// let btn1 = 0
+// if (btn1<=1200){
+//     $('.btn.left').click(function(){
+//         btn1 += 50
+//         $('#horse').css('right', `${btn1}px`)
+//     })
+// }
+// if (btn1<=0){
+//     $('.btn.right').click(function(){
+//         btn1 -= 50
+//         $('#horse').css('right', `${btn1}px`)
+//     })
+// }
